76f96526054afeb22f955c63c6366ab37898d5a2
Andie1810 <Hsu.andrea1810@gmail.com>
2026-03-24 08:30:17 +0000

    create html & js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add JavaScript below
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('posts');
+        data.slice(0,10).forEach(post => {
+            const postDiv = document.createElement('div');
+            postDiv.innerHTML = `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+            `;
+            container.appendChild(postDiv);
+        });
+    })
+
