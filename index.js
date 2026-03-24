554ee14edd2df0a3e3e577807b3585130761fb0d
Andie1810 <Hsu.andrea1810@gmail.com>
2026-03-24 09:11:35 +0000

    add css

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(data => {
         const container = document.getElementById('posts');
-        data.slice(0,10).forEach(post => {
+        data.slice(0,20).forEach(post => {
             const postDiv = document.createElement('div');
             postDiv.innerHTML = `
                 <h3>${post.title}</h3>
